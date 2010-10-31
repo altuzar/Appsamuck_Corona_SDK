51b615124333366a5a26e53b5a8b16d473188a3a
Altuzar <altuzzar@gmail.com>
2010-10-31 11:31:39 -0600

    Added comments to the example 1

--- 1/main.lua
+++ 1/main.lua
@@ -1,12 +1,19 @@
+--[[ This is the first example you can watch in: 
+          http://www.appsamuck.com/day1.html
+  ]] 
 
+-- Some vars. I didn't want to be cryptic
 local last, now, hour, min, sec
 
+-- Hide the Status Bar
 display.setStatusBar( display.HiddenStatusBar )
 
+-- Big textfield with Jack Bauer's 24 font. Don't know if works for Android
 local timeTextfield = display.newText( "00:00:00", 
 	display.contentWidth * 0.03, display.contentHeight * 0.38, "DBLCDTempBlack", 70 )
 timeTextfield:setTextColor(255,0,0)
 
+-- Main function. Only updated when time really changes
 displayTime = function ()
 	if last ~= os.date( "%c" ) then
 		last = os.date( "%c" )
@@ -18,4 +25,5 @@ displayTime = function ()
 	end
 end
 
+-- And this is the game loop
 Runtime:addEventListener( "enterFrame", displayTime )
